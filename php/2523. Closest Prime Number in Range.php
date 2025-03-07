e0691903ae0778d5af36c9a0bda642ba73718952
Puneet Garg <47375436+editerzframes@users.noreply.github.com>
2025-03-07 12:13:30 +0530

    Update 2523. Closest Prime Number in Range.php

--- php/2523. Closest Prime Number in Range.php
+++ php/2523. Closest Prime Number in Range.php
@@ -1,33 +1,42 @@
 <?php
 
-class Solution
-{
+class Solution {
 
     /**
      * @param Integer $left
      * @param Integer $right
      * @return Integer[]
      */
-    function closestPrimes($left, $right)
-    {
+    function closestPrimes($left, $right) {
+        
         $primeNumbers = array();
 
-        for ($i = $left; $i <= $right; $i++) {
-            if ($this->primeNumber($i)) {
-                $primeNumbers[] = $i;
+        $prime = array_fill(0, $right + 1, true);
+
+        for ($p = 2; $p*$p <= $right; $p++)
+        {
+            if ($prime[$p] == true)
+            {
+                for ($i = $p*$p; $i <= $right; $i += $p)
+                    $prime[$i] = false;
             }
         }
+    
+        // Print all prime numbers
+        for ($p = $left; $p <= $right; $p++)
+            if ($p > 1 && $prime[$p])
+                $primeNumbers[] = $p;
 
         $output = array();
-        if (count($primeNumbers) < 2) {
+        if(count($primeNumbers) < 2){
             $output = [-1, -1];
         }
         $difference = PHP_INT_MAX;
-        foreach ($primeNumbers as $index => $value) {
+        foreach($primeNumbers as $index => $value){
             $num1 = $value;
-            $num2 = $primeNumbers[$index + 1] ?? null;
-            if ($num2 != null) {
-                if (($num2 - $num1) < $difference) {
+            $num2 = $primeNumbers[$index+1] ?? null;
+            if($num2 != null){
+                if(($num2 - $num1) < $difference){
                     $output = [];
                     $output[] = $num1;
                     $output[] = $num2;
@@ -39,11 +48,10 @@ class Solution
         return $output;
     }
 
-    public function primeNumber($num)
-    {
+    public function primeNumber($num){
         $sqrtNum = floor(sqrt($num));
-        for ($i = 2; $i <= $sqrtNum; $i++) {
-            if ($num % $i == 0) {
+        for($i=2;$i<=$sqrtNum;$i++){
+            if($num%$i==0){
                 return false;
             }
         }
